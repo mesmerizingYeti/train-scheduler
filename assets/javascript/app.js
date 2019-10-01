57e6683d1163a589e545e14f6ca65e3104eb942c
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-10-01 01:00:49 -0700

    Added top bar  and styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAa4WTY--hLOfMFaQaoAzmnsCHv9cpRa7s",
+  authDomain: "train-scheduler-9bffd.firebaseapp.com",
+  databaseURL: "https://train-scheduler-9bffd.firebaseio.com",
+  projectId: "train-scheduler-9bffd",
+  storageBucket: "",
+  messagingSenderId: "331507088771",
+  appId: "1:331507088771:web:309189ad5d7669e31fcab6",
+  measurementId: "G-Q8NMLLX9VK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
